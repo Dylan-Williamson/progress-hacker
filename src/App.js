c22984c37f6b1da1b73e4a65ad1fae9412de3d8f
github username <61719449+Dylan-Williamson@users.noreply.github.com>
2021-07-16 13:50:10 -0400

    adds navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import Nav from './components/Nav';
 import HomePage from './containers/HomePage';
-import './App.css';
+import './styles/App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router';
 import { Switch, Route } from 'react-router-dom';
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import '../styles/Nav.css';
 
 const Nav = () => {
   return (
     <div className="nav">
-      
+      <div className="navWrapper">
+        <div className="username">
+          username
+        </div>
+        <div className="logo">
+          logo
+        </div>
+        <div className="balance">
+          balance
+        </div>
+      </div>
     </div>
   )
 }
